1ed0ea8ef6f60331a3e8148668d1b813b5bd66e7
logiclogue <jordanpaullord@hotmail.co.uk>
2017-11-24 12:18:00 +0000

    Adds cursor

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "cons.h"
+#include "Cursor.h"
 
 int main(void) {
     cons_test();
+    Cursor_test();
 
     FILE *file = fopen("src/main.c", "r");
     Cons *cons = cons_from_file(file);
