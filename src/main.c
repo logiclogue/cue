71442d5b8a53d5102a47f0f0aaff741d51b6adb7
logiclogue <jordanpaullord@hotmail.co.uk>
2017-11-24 08:41:57 +0000

    Adds example of reading in file, character by character

--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "cons.h"
 
-int main() {
+int main(void) {
+    cons_test();
+
+    FILE *file = fopen("src/main.c", "r");
+    char current_char;
+
+    while (current_char != EOF) {
+        current_char = fgetc(file);
+
+        printf("%c", current_char);
+    }
+
+    printf("All tests pass :)\n");
+
     return 0;
 }
