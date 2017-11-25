f6e959c30c8a93167cf48b71dc4369c7d140e3a6
logiclogue <jordanpaullord@hotmail.co.uk>
2017-11-25 20:38:55 +0000

    Removes curses test

--- src/main.c
+++ src/main.c
@@ -7,31 +7,6 @@
 #include "editor.h"
 #include "curses_interface.h"
 
-void curses_test(void) {
-    initscr();
-
-    printw("TESTING");
-    raw();
-    noecho();
-
-    refresh();
-
-    char c;
-
-    start_color();
-    init_pair(1, COLOR_RED, COLOR_BLACK);
-
-    while ((c = getch()) != 'J') {
-        attron(A_BOLD);
-        attron(COLOR_PAIR(1));
-        mvprintw(10, 10, "%c", c);
-        attroff(A_BOLD);
-        attroff(COLOR_PAIR(1));
-    }
-
-    endwin();
-}
-
 void test(void) {
     cons_test();
     cons_line_test();
